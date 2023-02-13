12c1bdf83dae77a54d507a606188a8fc356e9803
giacomosepe <74490368+giacomosepe@users.noreply.github.com>
2023-02-13 16:22:16 +0100

    changed structure of data for logos

---
+++ src/components/data/logos.js
@@ -0,0 +1,32 @@
+const Logos = [
+  {
+    name: "Fineurop",
+    src: "/src/assets/logos/Fineurop-logo.png",
+    width: "100",
+    height: "50",
+    url: "https://fineurop.it",
+  },
+  {
+    name: "Hines",
+    src: "/src/assets/logos/hines-logo.png",
+    width: "140",
+    height: "40",
+    url: "https://hines.com",
+  },
+  {
+    name: "Merrill Lynch",
+    src: "/src/assets/logos/merrill_lynch-logo.png",
+    width: "90",
+    height: "60",
+    url: "https://blackrock.com",
+  },
+  {
+    name: "UBS",
+    src: "/src/assets/logos/UBS-logo.png",
+    width: "120",
+    height: "35",
+    url: "https://ubs.com",
+  },
+];
+
+export default Logos;
