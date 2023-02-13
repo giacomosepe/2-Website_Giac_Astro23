752d120346e33509c0a344f839bd3448bc1da53a
giacomosepe <74490368+giacomosepe@users.noreply.github.com>
2023-02-13 12:02:03 +0100

    more test on logos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://2-website-giac-astro23.vercel.app/",
+  base: "/",
   integrations: [
     tailwind(),
     image({
