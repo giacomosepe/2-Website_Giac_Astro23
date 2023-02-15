ffc488db420b44056346b1233917a4f919cfa540
giacomosepe <74490368+giacomosepe@users.noreply.github.com>
2023-02-15 15:43:40 +0100

    added working form

--- src/components/data/logos.js
+++ src/components/data/logos.js
@@ -1,21 +1,21 @@
 const Logos = [
   {
     name: "Fineurop",
-    src: "./src/assets/logos/Fineurop-logo.png",
+    src: "assets/logos/Fineurop-logo.png",
     width: "100",
     height: "50",
     url: "https://fineurop.it",
   },
   {
     name: "Hines",
-    src: "src/assets/logos/hines-logo.png",
+    src: "./assets/logos/hines-logo.png",
     width: "140",
     height: "40",
     url: "https://hines.com",
   },
   {
     name: "Merrill Lynch",
-    src: "src/assets/logos/merrill_lynch-logo.png",
+    src: "assets/logos/merrill_lynch-logo.png",
     width: "90",
     height: "60",
     url: "https://blackrock.com",
