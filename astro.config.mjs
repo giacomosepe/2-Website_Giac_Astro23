d43dd86c0a3a9f74a640eb709a52f186e04d2f35
giacomosepe <74490368+giacomosepe@users.noreply.github.com>
2023-02-16 19:17:51 +0100

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   site: "https://giacomosepe.com/",
   base: "/",
   integrations: [
