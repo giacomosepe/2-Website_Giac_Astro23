201e9a99fc4010d0e0461a9db2deac7e606069ce
giacomosepe <74490368+giacomosepe@users.noreply.github.com>
2023-02-16 19:13:29 +0100

    updated to aastro2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://2-website-giac-astro23.vercel.app/",
+  site: "https://giacomosepe.com/",
   base: "/",
   integrations: [
     tailwind(),
--- src/components/data/logos.js
+++ src/components/data/logos.js
@@ -1,28 +1,28 @@
 const Logos = [
   {
     name: "Fineurop",
-    src: "assets/logos/Fineurop-logo.png",
+    src: "src/assets/fineurop-logo.png",
     width: "100",
     height: "50",
     url: "https://fineurop.it",
   },
   {
     name: "Hines",
-    src: "./assets/logos/hines-logo.png",
+    src: "./assets/hines-logo.png",
     width: "140",
     height: "40",
     url: "https://hines.com",
   },
   {
     name: "Merrill Lynch",
-    src: "assets/logos/merrill_lynch-logo.png",
+    src: "assets/merrill_lynch-logo.png",
     width: "90",
     height: "60",
     url: "https://blackrock.com",
   },
   {
     name: "UBS",
-    src: "/src/assets/logos/UBS-logo.png",
+    src: "/src/assets/UBS-logo.png",
     width: "120",
     height: "35",
     url: "https://ubs.com",
